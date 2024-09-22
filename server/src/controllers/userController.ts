57efacf22ed3eef3ae4ba045191443d33f6ce70a
ngdkhuong <nguyenkhuong31072002@gmail.com>
2024-09-22 19:17:57 +0700

    first commit

---
+++ server/src/controllers/userController.ts
@@ -0,0 +1,50 @@
+import { Request, Response } from "express";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export const getUsers  = async (req: Request, res: Response): Promise<void> => {
+    try {
+        const users = await prisma.user.findMany({})
+        res.status(200).json(users);
+    } catch (error: any) {
+        res.status(500).json({ message: `Error retrieving users: ${error.message}` });
+    }
+}
+
+export const getUser = async (req: Request, res: Response): Promise<void> => {
+    const {cognitoId} = req.params;
+    try {
+        const user = await prisma.user.findUnique({ where: { cognitoId } })
+        if (!user) {
+            res.status(404).json({ message: 'User not found' });
+        }
+        res.status(200).json(user);
+    } catch (error: any) {
+        res.status(500).json({ message: `Error retrieving users: ${error.message}` });
+    }
+}
+
+export const postUser = async (req: Request, res: Response): Promise<void>  => {
+    try {
+        const {
+          username,
+          cognitoId,
+          profilePictureUrl = "i1.jpg",
+          teamId = 1,
+        } = req.body;
+        const newUser = await prisma.user.create({
+          data: {
+            username,
+            cognitoId,
+            profilePictureUrl,
+            teamId,
+          },
+        });
+        res.json({ message: "User Created Successfully", newUser });
+    } catch (error: any) {
+    res
+        .status(500)
+        .json({ message: `Error retrieving users: ${error.message}` });
+    }
+}
